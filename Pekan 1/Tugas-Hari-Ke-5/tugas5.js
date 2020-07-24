68c3ec4ce47e67546f925652fb71e640c8dd4d25
danangpurbojati <danangpurbojati@gmail.com>
2020-07-24 08:32:49 +0700

    Tugas Hari Ke 5

---
+++ Pekan 1/Tugas-Hari-Ke-5/tugas5.js
@@ -0,0 +1,33 @@
+//Soal 1
+console.log("------SOAL 1 ------");
+function halo() {
+    return "Halo Sanbers";
+}
+
+console.log(halo());
+
+
+
+
+
+//Soal 2
+console.log("------SOAL 2 ------");
+function kalikan(num1, num2){
+    return num1 * num2;
+}
+
+var hasilKali = kalikan(8,6);
+console.log(hasilKali);
+
+
+
+
+
+//Soal 3
+console.log("------SOAL 3 ------");
+function introduce(name, age, address, hobby){
+    return "Nama Saya " + name + ", umur saya " + age + ", alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby;
+}
+
+var perkenalan = introduce("John", 30, "Jl makadam", "gaming");
+console.log(perkenalan);
